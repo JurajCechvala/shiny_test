d5bf13b2090ff2d8792cc5047f139dfe928d1261
Juraj Čechvala <juraj.cechvala@gmail.com>
2017-10-24 03:34:10 +0200

    Added a possible stress scenarios.

--- app.R
+++ app.R
@@ -9,6 +9,20 @@ yield.curve$fun <- approxfun(x = yield.curve$curve.data$term_Y,
                              y = yield.curve$curve.data$yield_bsp,
                              rule = 2)
 
+stress.fun <- list()
+
+stress.fun$no.stress  <- function(x) {rep(   0, length(x))}
+stress.fun$p200bps    <- function(x) {rep( 200, length(x))}
+stress.fun$m200bps    <- function(x) {rep(-200, length(x))}
+stress.fun$asc200bps  <- approxfun(x = c(0.5, 50),
+                                   y = c(0, 200),
+                                   rule = 2)
+stress.fun$desc200bps <- approxfun(x = c(0.5, 50),
+                                   y = c(200, 0),
+                                   rule = 2)
+names(stress.fun) <- c("No stress", "+200 bps uniform shift", "-200 bps uniform shift",
+                       "linear increase to +200 bps", "linear decrease from +200 bps")
+
 # SHINY APP ---------------------------------------------------------------------------------------
 # Define UI
 ui <-
@@ -26,7 +40,10 @@ ui <-
                   max = 2*max(yield.curve$curve.data$term_Y, na.rm = TRUE),
                   value = c(min(yield.curve$curve.data$term_Y, na.rm = TRUE),
                             max(yield.curve$curve.data$term_Y, na.rm = TRUE)),
-                  step = 1)),
+                  step = 1),
+      radioButtons("stress",
+                   "Select stress scenario",
+                   choices = names(stress.fun))),
     # Show a plot of the data
     mainPanel(
       plotOutput("ycPlot"))))
@@ -34,11 +51,18 @@ ui <-
 # Define server
 server <- function(input, output) {
 
+  function.to.plot <-
+    reactive({
+      function(x){
+        yield.curve$fun(x) + stress.fun[[input$stress]](x)
+      }
+    })
+
   output$ycPlot <-
     renderPlot({
       # draw the YC approximation function using ggplot
       ggplot(data.frame(x = c(input$window[1], input$window[2])), aes(x)) +
-        stat_function(fun = yield.curve$fun) +
+        stat_function(fun = function.to.plot()) +
         labs(x = "term [years]", y = "yield [bsp]")
     })
 }
